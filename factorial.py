48c9f4b7b37d609f3484bebb6d93e2de8bed0ca5
borntohtmlcss <borntohtmlcss@gmail.com>
2020-02-28 22:02:05 +0530

    latest update

--- factorial.py
+++ factorial.py
@@ -11,8 +11,6 @@ class gui:
         # controls start
         self.factorailbutton = Button(root, width="10", height="5", bg="#3498DB", text="Factorial",font="times 10 bold", command=self.factorialbutton)
         self.factorailbutton.pack(side=LEFT, padx=13, pady=13)
-        self.misbutton = Button(root, width="10", height="5", bg="#3498DB", text="Mis", font="times 10 bold",command=self.mis)
-        self.misbutton.pack(side=LEFT, padx=13, pady=13)
         self.validateemailbutton = Button(root, width="10", height="5", bg="#3498DB", text="Validate gmail", font="times 10 bold",command=self.gmail)
         self.validateemailbutton.pack(side=LEFT, padx=13, pady=13)
         self.countbutton = Button(root, width="10", height="5", bg="#3498DB", text="Word Count", font="times 10 bold",command=self.count)
@@ -45,58 +43,6 @@ class gui:
 
     # --------------------------------------------------factorial code ends ---------------------------------------------------------- -------
 
-    # --------------------------------------------------mis code start -----------------------------------------------------------------------
-    def mis(self):
-        root = Tk()
-        root.title("Allapps Mis")
-        root.propagate(0)
-        root.configure(background="#FBD28B",width=400,height=300)
-        self.namelable = Label(root, text="name:")
-        self.nameentry = Entry(root)
-        self.passwordlable = Label(root, text="password:")
-        self.passwordentry = Entry(root)
-        self.emaillable = Label(root, text="email:")
-        self.emailentry = Entry(root)
-        self.marks = Label(root,text="marks:")
-        self.markse = Entry(root)
-        self.output=Label(root,text="")
-        self.button = Button(root,text="submit",bg="#3498DB",command=self.btn)
-        self.namelable.place(x=10, y=10)
-        self.nameentry.place(x=70, y=10)
-        self.passwordlable.place(x=10, y=50)
-        self.passwordentry.place(x=70, y=50)
-        self.emaillable.place(x=10, y=100)
-        self.emailentry.place(x=70, y=100)
-        self.marks.place(x=10, y=150)
-        self.markse.place(x=70, y=150)
-        self.button.place(x=10, y=190)
-        self.output.place(x=10,y=220)
-        root.mainloop()
-
-    def btn(self):
-        name1=self.nameentry.get()
-        password1=self.passwordentry.get()
-        email=self.emailentry.get()
-        marks1=self.markse.get()
-        self.output.config(text="name is: "+name1+" ; password is: "+password1+" ; email id is: "+email+" ; marks is: "+marks1)
-        # Open database connection
-        db = pymysql.connect("localhost", "root", "", "python")
-
-        # prepare a cursor object using cursor() method
-        cursor = db.cursor()
-        # Prepare SQL query to INSERT a record into the database.
-        sql = "INSERT INTO `python`( `name`, `password`, `mail`, `marks`) VALUES (%s,%s,%s,%s)"
-        val = (name1,password1,email,marks1)
-
-        cursor.execute(sql,val)
-        db.commit()
-
-        cursor.execute("SELECT * from python")
-        rows=cursor.fetchall()
-        for echrows in rows:
-            print(echrows)
-
-# -----------------------------------------------------mis code ends----------------------------------------------------------------------
 
 # -----------------------------------------------------gmail code start ------------------------------------------------------------------
 
---
+++ redme.md
@@ -0,0 +1,9 @@
+# Basic python GUI application to calculate factorial , gmail validator , Word and characters Couter.
+
+![Test Image 1](front.PNG)
+
+![Test Image 1](factorial.png)
+
+
+![Test Image 1](wordcount.PNG)
+
